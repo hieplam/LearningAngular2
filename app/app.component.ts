350b8ecfcaf60333c2f30d8b66e5e085159338e3
lamhiep <lamhiep16@gmail.com>
2016-11-10 16:59:41 +0700

    Add Service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-
+import { ProductService } from './products/product.service';
 @Component({
     selector: 'pm-app',//pm stand for Project Management
     template: `
         <div><h1>{{pageTitle}}</h1>
             <pm-product></pm-product>
         </div>
-    `
-    // templateUrl: 'products/product-list.component'
+    `,
+    providers: [ProductService]
 })
 export class AppComponent {
     pageTitle: string = "Demo Angular";
